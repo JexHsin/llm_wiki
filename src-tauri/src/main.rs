d18fba9d786a0b8ef9e606c29c42782327075cf2
JexHsin <qingqingxiangzhu@163.com>
2026-06-24 15:24:59 +0800

    start public web API proxy before desktop runtime

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod web_api_proxy;
+
 fn main() {
+    web_api_proxy::start_web_api_proxy();
     llm_wiki_lib::run();
 }
